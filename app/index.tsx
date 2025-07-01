6c330bb9fa3b4b657ca43eb1bba2b82034e90ebd
Yorkshireman <>
2025-07-01 20:06:26 +0100

    move onPressReset() into a util

--- app/index.tsx
+++ app/index.tsx
@@ -1,39 +1,17 @@
-import * as Haptics from 'expo-haptics';
 import Ionicons from '@expo/vector-icons/Ionicons';
+import { onPressReset } from '../utils';
 import { useSetCountOnVolumeChange } from '../hooks';
-import { Alert, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function Index() {
   const { count, setCount } = useSetCountOnVolumeChange();
-
-  const onPressReset = () => {
-    if (count === 0) return;
-    Alert.alert(
-      'Reset',
-      'Are you sure you want to reset the counter to zero? This cannot be undone.',
-      [
-        {
-          style: 'cancel',
-          text: 'Cancel'
-        },
-        {
-          onPress: async () => {
-            setCount(0);
-            await Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Heavy);
-          },
-          text: 'OK'
-        }
-      ],
-      { cancelable: true }
-    );
-  };
 
   return (
     <View style={styles.container}>
       <Text adjustsFontSizeToFit={true} numberOfLines={1} style={styles.count}>
         {count}
       </Text>
-      <TouchableOpacity onPress={onPressReset} style={{ padding: 5 }}>
+      <TouchableOpacity onPress={() => onPressReset(count, setCount)} style={{ padding: 5 }}>
         <Ionicons color={'#fff'} name='refresh-outline' size={72} />
       </TouchableOpacity>
     </View>
---
+++ utils.ts
@@ -0,0 +1,27 @@
+import * as Haptics from 'expo-haptics';
+import { Alert } from 'react-native';
+
+export const onPressReset = (
+  count: number,
+  setCount: React.Dispatch<React.SetStateAction<number>>
+) => {
+  if (count === 0) return;
+  Alert.alert(
+    'Reset',
+    'Are you sure you want to reset the counter to zero? This cannot be undone.',
+    [
+      {
+        style: 'cancel',
+        text: 'Cancel'
+      },
+      {
+        onPress: async () => {
+          setCount(0);
+          await Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Heavy);
+        },
+        text: 'OK'
+      }
+    ],
+    { cancelable: true }
+  );
+};
